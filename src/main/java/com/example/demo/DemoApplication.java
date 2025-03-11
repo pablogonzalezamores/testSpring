b05aed981632fd858ed342e98db3e77497e83581
pablogonzalezamores <pablo.gonzalezamores@plexus.es>
2025-03-11 12:01:15 +0100

    Commit nº 5

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Commit nº 4");
+		System.out.println("Commit nº 5");
 	}
 
 }
